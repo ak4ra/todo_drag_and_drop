c8457dba2c8cd6bb66545a6e3ddfd3622e5d8f40
ak4ra <aryjna1@gmail.com>
2020-12-24 16:32:52 +0200

    A

--- todo_list.js
+++ todo_list.js
@@ -1,16 +1,24 @@
 const todoList = document.querySelector("#todo-list");
 
+
+const todoListItems = [
+	"placeholder list item text 1",
+	"placeholder list item text 2",
+	"placeholder list item text 3",
+	"placeholder list item text 4",
+];
+
 (function createListItem() {
-	for (let i = 0; i < 10; i++) {
+	for (const item of todoListItems) {
 		// create the list item container and h1 containing the todo text
 		const listItem = document.createElement("div");
-		listItem.classList.add("todo-list_item")
+		listItem.classList.add("todo-list_item");
 		const listItemText = document.createElement("h1");
-		listItemText.classList.add("todo-list_item_text")
-		const listItemTextNode = document.createTextNode("placeholder list item text " + i);
+		listItemText.classList.add("todo-list_item_text");
+		const listItemTextNode = document.createTextNode(item);
 		listItemText.appendChild(listItemTextNode);
 		listItem.appendChild(listItemText);
-		
+
 		// create the delete button and append it to the list item
 		const deleteButton = document.createElement("div");
 		deleteButton.classList.add("todo-list_item_delete-button");
@@ -18,9 +26,14 @@ const todoList = document.querySelector("#todo-list");
 		const deleteTextNode = document.createTextNode("X");
 		deleteButtonText.appendChild(deleteTextNode);
 		deleteButton.appendChild(deleteButtonText);
+		deleteButton.addEventListener("click", deleteListItem);
 		listItem.appendChild(deleteButton);
 
 		// add the list item to the todo list
 		todoList.appendChild(listItem);
 	}
 })();
+
+function deleteListItem(event) {
+	console.log(event);
+}
