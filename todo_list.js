2d52f15e157c99f72ff15ab3c8391cea858c8091
ak4ra <aryjna1@gmail.com>
2020-12-24 16:48:33 +0200

    delete item

--- todo_list.js
+++ todo_list.js
@@ -1,6 +1,5 @@
 const todoList = document.querySelector("#todo-list");
 
-
 const todoListItems = [
 	"placeholder list item text 1",
 	"placeholder list item text 2",
@@ -8,32 +7,31 @@ const todoListItems = [
 	"placeholder list item text 4",
 ];
 
-(function createListItem() {
-	for (const item of todoListItems) {
-		// create the list item container and h1 containing the todo text
-		const listItem = document.createElement("div");
-		listItem.classList.add("todo-list_item");
-		const listItemText = document.createElement("h1");
-		listItemText.classList.add("todo-list_item_text");
-		const listItemTextNode = document.createTextNode(item);
-		listItemText.appendChild(listItemTextNode);
-		listItem.appendChild(listItemText);
+function createListItem(item) {
+	// create the list item container and h1 containing the todo text
+	const listItem = document.createElement("div");
+	listItem.classList.add("todo-list_item");
+	const listItemText = document.createElement("h1");
+	listItemText.classList.add("todo-list_item_text");
+	const listItemTextNode = document.createTextNode(item);
+	listItemText.appendChild(listItemTextNode);
+	listItem.appendChild(listItemText);
 
-		// create the delete button and append it to the list item
-		const deleteButton = document.createElement("div");
-		deleteButton.classList.add("todo-list_item_delete-button");
-		const deleteButtonText = document.createElement("h1");
-		const deleteTextNode = document.createTextNode("X");
-		deleteButtonText.appendChild(deleteTextNode);
-		deleteButton.appendChild(deleteButtonText);
-		deleteButton.addEventListener("click", deleteListItem);
-		listItem.appendChild(deleteButton);
+	// create the delete button and append it to the list item
+	const deleteButton = document.createElement("div");
+	deleteButton.classList.add("todo-list_item_delete-button");
+	const deleteButtonText = document.createElement("h1");
+	const deleteTextNode = document.createTextNode("X");
+	deleteButtonText.appendChild(deleteTextNode);
+	deleteButton.addEventListener("click", () => listItem.remove());
+	deleteButton.appendChild(deleteButtonText);
+	listItem.appendChild(deleteButton);
 
-		// add the list item to the todo list
-		todoList.appendChild(listItem);
-	}
-})();
+	// add the list item to the todo list
+	todoList.appendChild(listItem);
+}
 
-function deleteListItem(event) {
-	console.log(event);
+// create list items for all current items
+for (const item of todoListItems) {
+	createListItem(item);
 }
